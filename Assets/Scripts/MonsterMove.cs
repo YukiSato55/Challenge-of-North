e3bebc71fb328d8dcc510873c7e87f178404bbf8
RenIshizaki0819 <43591944+RenIshizaki0819@users.noreply.github.com>
2018-10-25 11:49:14 +0900

    enemy作成途中

---
+++ Assets/Scripts/MonsterMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMove : MonoBehaviour {
+    private int MonsID;
+    private int MoveDirection = 0; // 動く方向 0：正面、1：右、2：左、3：後退
+    private float[] MonsterMoveSpeed = {0.01f, 0.02f, 0.03f};   
+
+	// Use this for initialization
+	void Start () {
+        MonsID = this.GetComponent<MonsterIDManager>().MonsterID;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if()
+		switch(MoveDirection)
+        {
+            case 0:
+                this.gameObject.transform.Translate(0, MonsterMoveSpeed[MonsID], 0);
+                break;
+        }
+	}
+
+    void Move()
+    {
+
+    }
+}
---
+++ Assets/Scripts/SeiseiManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeiseiManager : MonoBehaviour {
+    private Vector3 SeiseiPos;
+    private Vector3 pos;
+    private int monsID;
+
+	// Use this for initialization
+	void Start () {
+        pos = this.transform.position;
+        //Debug.Log("test" + Camera.main.ScreenToWorldPoint(pos));
+
+        // オブジェクトの座標を取得
+        SeiseiPos = Camera.main.ScreenToWorldPoint(pos);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Resources.UnloadUnusedAssets();
+    }
+
+    public void OnClick()
+    {
+        GameObject MonsObject = (GameObject)Resources.Load("Monster_0");// +monsID);
+        GameObject cloneObject = Instantiate(MonsObject, SeiseiPos, Quaternion.identity);
+    }
+}
