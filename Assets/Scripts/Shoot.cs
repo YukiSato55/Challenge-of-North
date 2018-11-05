2fb16f5731876537ba94b90c74f93bdfb907f0d9
RenIshizaki0819 <43591944+RenIshizaki0819@users.noreply.github.com>
2018-11-05 10:58:35 +0900

    enemy途中経過

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    private float chargeTime = 0;
+    private Vector3 pos;
+    private GameObject weapon;
+
+    // Use this for initialization
+    void Start()
+    {
+        pos = this.transform.position;
+        weapon = (GameObject)Resources.Load("Weapon/koji1_8");
+        Debug.Log(weapon);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        chargeTime += Time.deltaTime;
+
+        if (chargeTime >= 1.0f)
+        {
+            GameObject cloneobj = Instantiate(weapon, new Vector3(pos.x, pos.y, 0), Quaternion.identity);
+            chargeTime = 0;
+        }
+    }
+}
