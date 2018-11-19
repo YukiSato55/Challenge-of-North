dd436731b6704cabd1c81cd8de027c5a808c514a
RenIshizaki0819 <43591944+RenIshizaki0819@users.noreply.github.com>
2018-11-19 11:19:11 +0900

    11/19(yabai)

---
+++ Assets/Scripts/MoneyGauge.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyGauge : MonoBehaviour {
+    private float MoneyValue;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpDateGauge()
+    {
+
+    }
+}
