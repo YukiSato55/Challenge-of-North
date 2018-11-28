05c7587a93ec011d68826386c1acbeb59a4097f7
YukiSato55 <43591764+YukiSato55@users.noreply.github.com>
2018-11-28 16:19:59 +0900

    11/28

--- Assets/Scripts/SeiseiManager.cs
+++ Assets/Scripts/SeiseiManager.cs
@@ -4,62 +4,55 @@ using UnityEngine;
 
 //生成スクリプトUnity
 public class SeiseiManager : MonoBehaviour {
+	
     private Vector3 SeiseiPos;
-    private Vector3 pos;
+    private Vector3 Gotra;
     private int monsID;
 
 	public Canvas canvas;
-	//public int ID = 99;
+	int ID = 99;
 	int id = 99;
-	int a = 0;
-	private int ID
-	{
-		set {
-			Debug.Log (value + " set");
-			this.id = value; }
-		get {
-			Debug.Log (this.id + " get");
-			return this.id; }
-	}
-
-	//public int id;
 
 	// Use this for initialization
 	void Start () {
-		ID = id;
-        pos = this.transform.position;
-        //Debug.Log("test" + Camera.main.ScreenToWorldPoint(pos));
+		ID = 99;
+		//pos = posn.transform.position;
+		var Gotra = GameObject.Find("GoMonsterButton").transform;
+		Debug.Log("test" + Camera.main.ScreenToWorldPoint(Gotra));
 
-        // オブジェクトの座標を取得
-        SeiseiPos = Camera.main.ScreenToWorldPoint(pos);
+		// オブジェクトの座標を取得
+		SeiseiPos = Camera.main.ScreenToWorldPoint(Gotra);
+
+		Debug.Log (Gotra);
     }
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log ("a:" + a);
-       // Resources.UnloadUnusedAssets();
+		//Debug.Log ("a:" + a);
+       	Resources.UnloadUnusedAssets();
+		Debug.Log (id);
+		Debug.Log (ID);
 
     }
 
     public void OnClick()
     {
+		//Debug.Log (posn);
 
-       // GameObject MonsObject = (GameObject)Resources.Load("Monst/Monster_" + ID);// +monsID);
-       // GameObject cloneObject = Instantiate(MonsObject, new Vector3(SeiseiPos.x, SeiseiPos.y, 0), Quaternion.identity);
-		//cloneObject.gameObject.transform.parent = canvas.transform;
-
+        GameObject MonsObject = (GameObject)Resources.Load("Monst/Monster_" + ID);// +monsID);
+        GameObject cloneObject = Instantiate(MonsObject, new Vector3(SeiseiPos.x, SeiseiPos.y, 0), Quaternion.identity);
+		cloneObject.gameObject.transform.parent = canvas.transform;
     }
 
 	//キャラ選択
 	public void Change(int num){
-		a++;
-		Debug.Log ("a " + a);
-		Debug.Log (id);
+		//a++;
+		//Debug.Log ("a " + a);
+		//Debug.Log (id);
+		//Debug.Log (id);
+		Debug.Log (ID + ":" + num);
 		ID = num;
-		Debug.Log (id);
-		//Debug.Log (ID + ":" + num);
-		//ID = num;
-		//Debug.Log (ID + ":" + num);
+		Debug.Log (ID + ":" + num);
 	}
 
 }
