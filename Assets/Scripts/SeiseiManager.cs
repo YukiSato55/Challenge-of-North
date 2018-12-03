901efa06518b0dede76bb049157d19cbc5aaebea
YukiSato55 <43591764+YukiSato55@users.noreply.github.com>
2018-12-03 09:42:52 +0900

    11/30

--- Assets/Scripts/SeiseiManager.cs
+++ Assets/Scripts/SeiseiManager.cs
@@ -6,24 +6,35 @@ using UnityEngine;
 public class SeiseiManager : MonoBehaviour {
 	
     private Vector3 SeiseiPos;
-    private Vector3 Gotra;
+    //private Vector3 pos;
     private int monsID;
 
 	public Canvas canvas;
 	int ID = 99;
 	int id = 99;
 
+	private GameObject GoMonsterButton;
+	private Vector3 seisePos;
+
 	// Use this for initialization
 	void Start () {
 		ID = 99;
-		//pos = posn.transform.position;
-		var Gotra = GameObject.Find("GoMonsterButton").transform;
-		Debug.Log("test" + Camera.main.ScreenToWorldPoint(Gotra));
+		//pos = tmp.transform.position;
+		//座標取得、代入
+		var tmp = GameObject.Find("GoMonsterButton").transform.position;
+		GameObject.Find ("GoMonsterButton").transform.position = new Vector3 (tmp.x, tmp.y, tmp.z);
+
+		GoMonsterButton = GameObject.Find ("GoMonsterButton");
+
+		Debug.Log ("tttt " + seisePos);
+
+
+		Debug.Log("test" + Camera.main.ScreenToWorldPoint(tmp));
 
 		// オブジェクトの座標を取得
-		SeiseiPos = Camera.main.ScreenToWorldPoint(Gotra);
+		SeiseiPos = Camera.main.ScreenToWorldPoint(tmp);
 
-		Debug.Log (Gotra);
+		Debug.Log (tmp);
     }
 	
 	// Update is called once per frame
@@ -36,12 +47,20 @@ public class SeiseiManager : MonoBehaviour {
     }
 
     public void OnClick()
-    {
-		//Debug.Log (posn);
+	{	
+		
+		Vector3 seisePos = Camera.main.ScreenToWorldPoint (GoMonsterButton.transform.position);
+		seisePos.z = 0;
 
+		//Debug.Log (posn);
         GameObject MonsObject = (GameObject)Resources.Load("Monst/Monster_" + ID);// +monsID);
-        GameObject cloneObject = Instantiate(MonsObject, new Vector3(SeiseiPos.x, SeiseiPos.y, 0), Quaternion.identity);
-		cloneObject.gameObject.transform.parent = canvas.transform;
+
+		//GameObject cloneObject = Instantiate(MonsObject, new Vector3(SeiseiPos.x, SeiseiPos.y, 0), Quaternion.identity);
+		//GameObject cloneObject = Instantiate(MonsObject, seisePos, Quaternion.identity);
+		GameObject cloneObject = (GameObject)Instantiate(MonsObject);
+		cloneObject.gameObject.transform.SetParent (canvas.transform, false);
+		cloneObject.transform.position = seisePos;
+		//cloneObject.gameObject.transform.parent = canvas.transform;
     }
 
 	//キャラ選択
