f4c8b2e3adf325866db1ce78599e04efa91217bc
RenIshizaki0819 <43591944+RenIshizaki0819@users.noreply.github.com>
2019-01-17 12:26:18 +0900

    1/17(2)

---
+++ Assets/Scripts/ButtonActive.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActive : MonoBehaviour {
+    [SerializeField] // 表示非表示換える用
+    private GameObject obj;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void Active()
+    {
+        obj.SetActive(true);
+    }
+
+    public void Inactive() // 非表示用
+    {
+        obj.SetActive(false);
+    }
+
+}
