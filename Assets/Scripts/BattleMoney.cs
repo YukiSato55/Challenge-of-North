eca4da43fcef917dcbd3960099664735a6ea57da
RenIshizaki0819 <43591944+RenIshizaki0819@users.noreply.github.com>
2019-01-21 16:59:29 +0900

    1/21(最終)

---
+++ Assets/Scripts/BattleMoney.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleMoney : MonoBehaviour {
+    private Text MoneyText;
+    private float nowMoney;
+
+	// Use this for initialization
+	void Start () {
+        MoneyText = GetComponent<Text>();
+		if(PlayerPrefs.HasKey("Money"))
+        {
+            nowMoney = PlayerPrefs.GetFloat("Money");
+        } else
+        {
+            nowMoney = 0;
+        }
+        MoneyText.text = "所持金:￥" + nowMoney;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
