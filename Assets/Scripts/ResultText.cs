a2210be5ffd3eaf576076a5825c08bdb1421f5f7
RenIshizaki0819 <43591944+RenIshizaki0819@users.noreply.github.com>
2019-01-29 15:02:38 +0900

    Revert "Revert "1/29(やっちゃった)""
    This reverts commit c432560c286643678ef2f3eabb00f80369ac9583.

--- Assets/Scripts/ResultText.cs
+++ Assets/Scripts/ResultText.cs
@@ -5,19 +5,29 @@ using UnityEngine.UI;
 
 public class ResultText : MonoBehaviour {
     private Text maintext;
+    [SerializeField]
+    private Text SubBreaktext, SubCosttext;
 
 	// Use this for initialization
 	void Start () {
+        Debug.Log(SubCosttext + "" + SubBreaktext);
         maintext = GetComponent<Text>();
         maintext.text = "全滅した...";
-	}
+        SubBreaktext.text = "0%";
+        SubCosttext.text = "0/0";
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
 	}
 
-    public void Clear()
+    public void Clear(float NorumaCost)
     {
         maintext.text = "基地を破壊した!!";
+        SubBreaktext.text = "100%";
+        SubCosttext.text = GameObject.Find("SUMBuyMoney").GetComponent<SUMBuyMoney>().SUM + "/" + NorumaCost;
     }
 }
--- Assets/Scripts/SUMBuyMoney.cs
+++ Assets/Scripts/SUMBuyMoney.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SUMBuyMoney : MonoBehaviour {
-    private float SUM;
+    public float SUM;
 
 	// Use this for initialization
 	void Start () {
