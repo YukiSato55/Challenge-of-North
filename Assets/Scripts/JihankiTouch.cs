4527172646be28171df66d11e6dfa7a4ab10674d
RenIshizaki0819 <43591944+RenIshizaki0819@users.noreply.github.com>
2019-02-04 09:40:35 +0900

    2/1

--- Assets/Scripts/JihankiTouch.cs
+++ Assets/Scripts/JihankiTouch.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class JihankiTouch : MonoBehaviour {
+    [SerializeField]
+    private GameObject gameObject;
+    private Vector3 pos;
+    private 
 
 	// Use this for initialization
 	void Start () {
-		
+        pos = this.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void Form()
+    {
+        //Instantiate(gameObject, new Vector3(pos.x, pos.y, pos.z), Quaternion.identity, );
+    }
 }
